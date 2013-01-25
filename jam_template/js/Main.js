3fbaa0fef107cf57dff0be95ea9b781801a17676
Andrew Blackbourn <ablackbourn@mrx.ca>
2013-01-25 12:53:08 -0500

    what was I doing again?

--- jam_template/js/Main.js
+++ jam_template/js/Main.js
@@ -1,3 +1,4 @@
+var sprites = [];
 Main = function() {
     /*
      * Modified version of http://active.tutsplus.com/tutorials/games/learn-createjs-by-building-an-html5-pong-game/
@@ -6,51 +7,65 @@ Main = function() {
         /* Link Canvas */
         
         this.canvas = document.getElementById('canvas');
-        this.stage = new Stage(canvas);
-        this.stage.mouseEventsEnabled = true;
-        
+        var stage = new Stage(canvas);
+        stage.mouseEventsEnabled = true;
+        stage.onMouseDown = function() {};
+        stage.onMouseMove = function() {};
+        stage.onMouseUp = function() {};
         /* Set The Flash Plugin for browsers that don't support SoundJS */
         SoundJS.FlashPlugin.BASE_PATH = "assets/soundjs_flashplugin";
         if (!SoundJS.checkPlugin(true)) {
           alert("Error!");
           return;
         }
+        var imgDir = "assets/image/";
+        var sndDir = "assets/audio/";
         var manifest = [
-                    {src:"assets/image/paddle.png", id:"player"},
-                    {src:"assets/audio/hit.mp3|assets/audio/hit.ogg", id:"hit"}
-                ];
+            {src: imgDir+"paddle.png", id:"player"},
+            {src: sndDir+"hit.mp3|"+sndDir+"/hit.ogg", id:"hit"}
+        ];
 
 
 
         var preloader = new PreloadJS();
         preloader.installPlugin(SoundJS);
-        var preloadHandler = new PreloaderHandler(manifest);
+        var preloadHandler = new PreloaderHandler(manifest, stage);
         preloader.onProgress = preloadHandler.handleProgress;
         preloader.onComplete = preloadHandler.handleComplete;
         preloader.onFileLoad = preloadHandler.handleFileLoad;
-        preloader.loadManifest(manifest);
-
-        /* Ticker */
-        
-        Ticker.setFPS(30);
-        Ticker.addListener(this.stage);
+        preloader.loadManifest(manifest, stage);
     }
 
     // Main Loop
 
     this.update = function() {
+        alert('update');
     }
 };
 
-PreloaderHandler = function(manifest) {
+PreloaderHandler = function(manifest, stage) {
     var totalLoaded = 0;
     var self = this;
-    this.handleProgress = function(event) {
+    var progress = 0;
+    var progressText = new Text('0%', "12px Arial", "orange");
+    console.log(stage);
+    progressText.x = 299;
+    progressText.y = 50;
+    stage.addChild(progressText);
+    var updateProgress = function(event) {
         //use event.loaded to get the percentage of the loading
+        if (event.loaded) {
+            progressText.text = event.loaded/1*100+"%";
+        }
+        stage.update();
+    }
+    this.handleProgress = function(event) {
+        updateProgress(event);
     }
 
     this.handleComplete = function (event) {
         //triggered when all loading is complete
+        updateProgress(event);
     }
 
     this.handleFileLoad = function(event) {
@@ -62,7 +77,7 @@ PreloaderHandler = function(manifest) {
                 var img = new Image();
                 img.src = event.src;
                 img.onload = self.handleLoadComplete;
-                window[event.id] = new Bitmap(img);
+                sprites[event.id] = new Bitmap(img);
             break;
             case PreloadJS.SOUND:
                 //sound loaded
@@ -70,11 +85,21 @@ PreloaderHandler = function(manifest) {
             break;
         }
     }
+    
     this.handleLoadComplete = function(event) {
         totalLoaded++;
 
         if(manifest.length==totalLoaded) {
-            alert('yay');
         }
     }
+}
+
+MainMenu = function(stage) {
+    /* Ticker */
+    var ticker = new Object();
+    Ticker.setFPS(30);
+    Ticker.addListener(this.stage);
+    Ticker.addListener(tkr, false);
+    Ticker.removeListener(tkr);
+    ticker.tick = self.update;
 }
